1aac6b5c2f639422433de159122bf22923647cdf
Serg Kostyuk <serg.kostyuk@gmail.com>
2025-08-20 10:15:51 -0400

    All fixed

--- src/components/DemoInfoPanel.vue
+++ src/components/DemoInfoPanel.vue
@@ -27,7 +27,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg>
-                                Demo Site Information
+                                Blog Platform Demo
                             </h2>
 
                             <div class="grid md:grid-cols-2 gap-8 mb-8">
